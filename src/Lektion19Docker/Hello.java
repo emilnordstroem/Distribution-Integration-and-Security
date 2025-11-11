e8d1a8b469f6582ca04e7877166495cfc938b9cc
emilnordstroem <emilstoeve@outlook.com>
2025-11-11 09:39:59 +0100

    Docker demo

---
+++ src/Lektion19Docker/Hello.java
@@ -0,0 +1,12 @@
+package Lektion19Docker;
+
+public class Hello {
+
+    // Vær obs. på at de to filer: Dockerfile og Hello skal lægges ind i et nyt projekt for at virke
+    // Se eventuelt DockerTest projektet
+
+    public static void main(String[] args){
+        System.out.println("This is java app \n by using Docker");
+    }
+
+}
